1e6594716f8be666ba99e2b366d71bd16f48c84f
Roberto Dyke <roberto.dyke@inria.fr>
2024-06-11 16:20:41 +0200

    CGAL test (removed dependency from Mac OS and Windows to see whether it is necessary)

--- plugins/core/Standard/MyPlugin/src/MyPlugin.cpp
+++ plugins/core/Standard/MyPlugin/src/MyPlugin.cpp
@@ -21,6 +21,13 @@
 
 #include "ActionA.h"
 
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/convex_hull_2.h>
+#include <vector>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point_2;
+typedef std::vector<Point_2> Points;
+
 // Default constructor:
 //	- pass the Qt resource path to the info.json file (from <yourPluginName>.qrc file) 
 //  - constructor should mainly be used to initialize actions and other members
@@ -72,6 +79,15 @@ QList<QAction *> MyPlugin::getActions()
 		connect( m_action, &QAction::triggered, this, [this]()
 		{
 			Example::performActionA( m_app );
+
+			Points points, result;
+			points.push_back(Point_2(0,0));
+			points.push_back(Point_2(10,0));
+			points.push_back(Point_2(10,10));
+			points.push_back(Point_2(6,5));
+			points.push_back(Point_2(4,1));
+			CGAL::convex_hull_2( points.begin(), points.end(), std::back_inserter(result) );
+			std::cout << result.size() << " points on the convex hull" << std::endl;
 		});
 	}
 
